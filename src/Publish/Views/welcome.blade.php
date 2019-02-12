d4ab54267b4d3cb23dd0d109c32beb3b94e1f603
rafo.abbas <rafo.abbas@gmail.com>
2019-02-12 20:27:22 +0300

    Change View and Style

--- src/Publish/Views/welcome.blade.php
+++ src/Publish/Views/welcome.blade.php
@@ -1,7 +1,41 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: rauf
- * Date: 2/12/2019
- * Time: 3:05 PM
- */
+
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link type="text/css" rel="stylesheet" href="{{thasset('css/default.css')}}"/>
+
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+                <a href="{{ url('/home') }}">Home</a>
+            @else
+                <a href="{{ route('login') }}">Login</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}">Register</a>
+                @endif
+            @endauth
+        </div>
+    @endif
+
+    <div class="content">
+        <div class="title m-b-md">
+            kubpro/theme
+        </div>
+
+    </div>
+</div>
+</body>
+</html>
